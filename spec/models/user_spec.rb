06d5fbb34a8f47332ad64a8f8a4d5e22ea1c43dd
Dinshaw <gobhai@gmai.com>
2020-08-04 16:56:40 -0600

    Scope SearchController to @current_user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,4 +2,5 @@ RSpec.describe User do
   it { is_expected.to validate_presence_of :email }
   it { is_expected.to validate_presence_of :password }
   it { is_expected.to validate_confirmation_of :password }
+  it { is_expected.to have_many :searches }
 end
